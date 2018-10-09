3756af984c3d3fbe8ee02b6b773c10f83785e080
Jonathan Bispo <jfb0101@gmail.com>
2018-10-09 20:19:20 -0300

    cadastro de roles na área administrativa ok

--- Areas/Admin/Controllers/RolesController.cs
+++ Areas/Admin/Controllers/RolesController.cs
@@ -1,10 +1,31 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Sakila.Admin.Models;
+using Sakila.Security;
 
 namespace Sakila.Admin.Controllers {
     [Area("Admin")]
     public class RolesController : Controller {
+        private RoleManager<ApplicationRole> roleManager;
+
+        public RolesController(RoleManager<ApplicationRole> roleManager) {
+            this.roleManager = roleManager;
+        }
+
         public IActionResult CreateOrUpdate() {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrUpdate(CreateUpdateRoleViewModel model) {
+            await roleManager.CreateAsync(new ApplicationRole(){
+                Name = model.Name
+            });
+
+            model.Created = true;
+
+            return View(model);
         }
     }
 }
---
+++ Areas/Admin/Models/CreateUpdateRoleViewModel.cs
@@ -0,0 +1,6 @@
+namespace Sakila.Admin.Models {
+    public class CreateUpdateRoleViewModel {
+        public string Name {get;set;}
+        public bool? Created {get;set;}
+    }
+}
